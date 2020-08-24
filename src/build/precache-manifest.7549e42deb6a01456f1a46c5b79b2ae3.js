3bba35e089af417fd5410744ad8d0e372f32b1c0
Nejmeh Jarjoura <nejmehjarjoura@gmail.com>
2020-08-24 21:55:46 +0300

    First commit

---
+++ src/build/precache-manifest.7549e42deb6a01456f1a46c5b79b2ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6684ae48e77c31411edd6e200cb33f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36dc6b8e2a7f2ffffa32",
+    "url": "/static/css/main.7fe9d009.chunk.css"
+  },
+  {
+    "revision": "9f27527568c48733aa8c",
+    "url": "/static/js/2.ca0bc356.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca0bc356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36dc6b8e2a7f2ffffa32",
+    "url": "/static/js/main.e8f3c9e0.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
